b713b8cbfc6ddb8dc2346554a8e4124a721c5534
kuku <109440295+kukuos@users.noreply.github.com>
2026-07-12 18:38:39 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   // Standalone build for Cloudflare Pages
   // Note: Most routes use 'edge' runtime which is compatible with Cloudflare
-  output: 'undefined',
+  output: 'standalone',
 
   // Don't fail build on ESLint warnings
   eslint: {
